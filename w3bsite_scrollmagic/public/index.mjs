c83cb7251ba688247951d902c3c066d1240c806c
Exr0n <mail@exr0n.com>
2022-07-24 02:35:47 -0700

    super basic scrollmagic demo

--- w3bsite_scrollmagic/public/index.mjs
+++ w3bsite_scrollmagic/public/index.mjs
@@ -1,2 +1,25 @@
-console.log("hello world")
+console.log("hello world");
 
+function setup_scrollmagic() {
+    console.log("ayoo???")
+    const controller = new ScrollMagic.Controller();
+    console.log(controller)
+
+    var blockTween = new TweenMax.to('#block', 1.5, {
+        backgroundColor: 'red'
+    });
+    console.log("ayoo???")
+
+    var containerScene = new ScrollMagic.Scene({
+        triggerElement: '#container'
+    })
+        .setTween(blockTween)
+        .addIndicators()
+        .addTo(controller);
+
+    console.log("ayoo???")
+
+    console.log(containerScene)
+}
+
+document.addEventListener('DOMContentLoaded', setup_scrollmagic)
