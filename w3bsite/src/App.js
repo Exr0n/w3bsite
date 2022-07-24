2beac0708faee94d5b39835e6df75b638eef03a9
Exr0n <mail@exr0n.com>
2022-07-24 02:15:45 -0700

    init new website with tailwind

--- w3bsite/src/App.js
+++ w3bsite/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Markdown from 'marked-react';
+import { Controller, Scene } from 'react-scrollmagic';
 
 import Fade from 'react-reveal/Fade';
 
@@ -21,33 +22,45 @@ const sections = {
 
 function App() {
     return (
-        <div className="flex-col w-screen h-screen overflow-x-hidden overflow-y-scroll bg-dark text-primary">
-            <div className="h-screen">
-        hewo
+            <div>
+                <Controller>
+                    <Scene duration={600} pin>
+                        <div>Sticky Example</div>
+                    </Scene>
+                </Controller>
             </div>
-            <div className="w-5/6 m-auto">
-            {
-                Object.entries(sections).map(([sec_title, items]) => <div className="flex flex-col">{
-                    Object.values(items).map(({title, description}) => {
-
-                        console.log(description)
-                        return <>
-                            <div className="flex items-center h-screen p-2 border-2 border-primary">
-                                <Fade bottom>
-                                    <div className="justify-center border-4 border-primary max-w-prose">
-                                        <span className="text-xl font-bold">{title}</span>
-                                        <div><Markdown value={description} /></div>
-                                    </div>
-                                </Fade>
-                            </div>
-                        </>
-                    })}
-                    </div>
-                )
-            }
-            </div>
-        </div>
-    );
+    )
 }
+
+//function App() {
+//    return (
+//        <div className="flex-col w-screen h-screen overflow-x-hidden overflow-y-scroll bg-dark text-primary">
+//            <div className="h-screen">
+//        hewo
+//            </div>
+//            <div className="w-5/6 m-auto">
+//            {
+//                Object.entries(sections).map(([sec_title, items]) => <div className="flex flex-col">{
+//                    Object.values(items).map(({title, description}) => {
+//
+//                        console.log(description)
+//                        return <>
+//                            <div className="flex items-center h-screen p-2 border-2 border-primary">
+//                                <Fade bottom>
+//                                    <div className="justify-center border-4 border-primary max-w-prose">
+//                                        <span className="text-xl font-bold">{title}</span>
+//                                        <div><Markdown value={description} /></div>
+//                                    </div>
+//                                </Fade>
+//                            </div>
+//                        </>
+//                    })}
+//                    </div>
+//                )
+//            }
+//            </div>
+//        </div>
+//    );
+//}
 
 export default App
---
+++ w3bsite_scrollmagic/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'dark': '#121019',
+            'primary': '#a5d2ff',
+            'accent': '#326ccc',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
