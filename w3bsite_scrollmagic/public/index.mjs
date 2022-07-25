c0c630ad0007dc0a0427b67a4ff9f26e81423fb0
Exr0n <mail@exr0n.com>
2022-07-25 01:53:57 -0700

    timed tweens

--- w3bsite_scrollmagic/public/index.mjs
+++ w3bsite_scrollmagic/public/index.mjs
@@ -1,3 +1,4 @@
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js';
 console.log("hello world");
 
 const sections = {
@@ -13,83 +14,52 @@ const sections = {
     ]
 }
 
-function el(elem_name, inline_style, ...children) {
-    //if (children.includes(',')) console.log(children);
-    const return_el = `<${elem_name} style="${inline_style}">\n${children.join("\n")}\n</${elem_name}>`;
-    if (return_el.includes(',')) console.log(children[0]);
-    return return_el;
-    //const e = typeof type_or_constructor === 'string' ? document.createElement(type_or_constructor) : type_or_constructor();
-    //e.classList.add(classList.split(' '));
-    //for (items of children) e.appendChild(children);
-    //return e;
+function el(elem_name, inline_style, children, className=null) {
+    return `<${elem_name}${className ? ' class=' + className : ''} style="${inline_style}">\n${children.join("\n")}\n</${elem_name}>`;
+}
+
+function zip(...arr) {
+    const min_len = Math.min(arr.map(a => a.length));
+    return arr[0].map((_, i) => arr.map(row => row[i]));    // https://stackoverflow.com/a/10284006/10372825
 }
 
 function budget_react_inital_render() {
     const inner_html = el('div', null, 
-        ...Object.entries(sections).map(([sec_title, items]) => el('div', 'display: flex; flex-direction: column;',
-            ...Object.values(items).map(({title, description}) => el('div', 'display: flex; align-items: center; height: 100vh; padding: 2em; border: 2px solid red;',
-                el('div', 'justify-content: center; border: 4px solid green; max-width: 65ch;', 
-                    el('span', 'size: 3em bold;', title),
-                    el('div', null, DOMPurify.sanitize(marked.parse(description)))
-                )
-            ))
+        Object.entries(sections).map(([sec_title, items]) => el('div', 'display: flex; flex-direction: column;',
+            Object.values(items).map(({title, description}) => el('div', 'display: flex; align-items: center; height: 100vh; padding: 2em; border: 2px solid red;', [
+                el('div', 'justify-content: center; border: 4px solid green; max-width: 65ch;', [
+                    el('span', 'font: 2em sans-serif; font-weight: bold;', [title]),
+                    el('div', null, [DOMPurify.sanitize(marked.parse(description))])
+                ], 'has-line-art')
+            ]))
         ))
     );
 
     console.log(inner_html)
-
-
-    //const sections_frag = document.createDocumentFragment();
-    //
-    //Object.entries(sections).forEach(([sec_title, items]) => {
-    //    const item_div = el('div', 'flex flex-col');
-    //
-    //    sections_frag.appendChild(item_div);
-    //});
-    //<div className="flex flex-col">{
-    //    Object.values(items).map(({title, description}) => {
-    //
-    //        console.log(description)
-    //        return <>
-    //            <div className="flex items-center h-screen p-2 border-2 border-primary">
-    //                <Fade bottom>
-    //                    <div className="justify-center border-4 border-primary max-w-prose">
-    //                        <span className="text-xl font-bold">{title}</span>
-    //                        <div><Markdown value={description} /></div>
-    //                    </div>
-    //                </Fade>
-    //            </div>
-    //        </>
-    //    })}
-    //    </div>
-    //)
     
     const container = document.getElementById('line-main');
     container.innerHTML = inner_html;
 }
 
 function setup_scrollmagic() {
-    console.log("ayoo???")
     const controller = new ScrollMagic.Controller();
-    console.log(controller)
 
-    var blockTween = new TweenMax.to('#block', 0.1, {
-        backgroundColor: 'red'
-    });
-    console.log("ayoo???")
+    const line_art_els = Array.from(document.getElementsByClassName('has-line-art'));
+    console.log(line_art_els)
 
-    var containerScene = new ScrollMagic.Scene({
-        triggerElement: '#line-main',
-        duration: 500,
+    const tweens = line_art_els.map(el => new TweenMax.to(el, 0.1, { backgroundColor: 'red' }));
+    console.log(tweens)
+
+    const scenes = zip(line_art_els, tweens).map(([el, tween]) => new ScrollMagic.Scene({
+        triggerElement: el,
+        duration: el.clientHeight,
         tweenChanges: true,
     })
-        .setTween(blockTween)
+        .setTween(tween)
         .addIndicators()
-        .addTo(controller);
-
-    console.log("ayoo???")
-
-    console.log(containerScene)
+        .addTo(controller)
+    );
+    console.log(scenes)
 }
 
 function main() {
